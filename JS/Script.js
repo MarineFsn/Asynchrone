5543a87723b5dea6a1b68cc3c433185c59ec2772
fassma89@gmail.com <fassma89@gmail.com>
2023-05-16 15:50:44 +0200

    Exercice 1 trying again

--- JS/Script.js
+++ JS/Script.js
@@ -1,19 +1,17 @@
-const request = fetch("./Assets/your-json-file.json");
-console.log(request); 
-
-const response = request.then((response) => response.text());
-console.log(response); 
-
-response.then((text) => {
-	console.log(text);
-
-const heroesList = document.createElement("p");
-
-
-document.body.appendChild(heroesList);
-
+fetch("./Assets/your-json-file.json")
+.then (response => response.json())
+.then(data => {
+    console.log(data);
 
 });
+
+
+
+
+
+
+
+
 
 
 
