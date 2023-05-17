77aa5d688a19ac5857c142edc7ef357c07700d75
fassma89@gmail.com <fassma89@gmail.com>
2023-05-17 09:57:34 +0200

    Exercice 1 ended

--- JS/Script.js
+++ JS/Script.js
@@ -1,27 +1,16 @@
-fetch("./Assets/your-json-file.json")
-.then (response => response.json())
-.then(data => {
-    console.log(data);
+document.getElementById("dynamicBtn").addEventListener("click", function() {
+  fetch("./Assets/your-json-file.json")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      let ul = document.createElement("ul");
 
+      data.members.forEach(item => {
+        let li = document.createElement("li");
+        li.textContent = item.name;
+        ul.appendChild(li);
+      });
+
+      document.body.appendChild(ul);
+    })
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
